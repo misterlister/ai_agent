a4806aac50e80c1fcb169f3b558f09eea8b83625
misterlister <haydenglister@gmail.com>
2025-08-15 17:10:47 -0700

    Updated main to use command line argument to specify prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,7 +9,12 @@ client = genai.Client(api_key = api_key)
 
 def main():
     model_name = "gemini-2.0-flash-001"
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+    if len(sys.argv) < 2:
+        print("Error: no prompt provided")
+        sys.exit(1)
+    
+    prompt = sys.argv[1]
     
     response = client.models.generate_content(model = model_name, contents = prompt)
 
