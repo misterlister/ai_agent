6e2326884249ec96452c1d1860c5bada4caa707c
misterlister <haydenglister@gmail.com>
2025-08-20 12:06:59 -0700

    Added ability to get function calls back from agent, based on prompts

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
@@ -31,15 +32,40 @@ def main():
 
         
 def generate_content(client, messages, verbose):
+    
+    system_prompt="""
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+    
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info
+        ]
+    )
+    
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
-        contents = messages
+        contents = messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt),
     )
 
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
     
-    print(response.text)
+    if len(response.function_calls) > 0:
+        for function_call in response.function_calls:
+            
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
     
     if verbose:
         print(f"Prompt tokens: {prompt_tokens}")
